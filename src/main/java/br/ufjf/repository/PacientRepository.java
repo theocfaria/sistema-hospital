fd23e23e59f4e0185847bc9b7986cb69368f5cfc
Theo Faria <theocfaria@gmail.com>
2025-12-21 18:58:35 -0300

    feat: adiciona load e save de pacientes

--- src/main/java/br/ufjf/repository/PacientRepository.java
+++ src/main/java/br/ufjf/repository/PacientRepository.java
@@ -1,10 +1,53 @@
 package br.ufjf.repository;
 
+import java.lang.reflect.Type;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+
 import br.ufjf.model.Pacient;
 
 class PacientRepository {
-    private String filepath = "pacients.json";
+    private final String FILEPATH = "pacients.json";
+    private final Gson gson;
+
+    public PacientRepository() {
+        this.gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    public void saveAll(List<Pacient> list) {
+        try (FileWriter writer = new FileWriter(FILEPATH)) {
+            gson.toJson(list, writer);
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar.");
+        }
+    }
+
+    public List<Pacient> loadAll() {
+        File file = new File(FILEPATH);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (FileReader reader = new FileReader(FILEPATH)) {
+            // isso aqui é pra salvar o tipo da lista durante o runtime
+            Type listType = new TypeToken<ArrayList<Pacient>>() {
+            }.getType();
+            List<Pacient> list = gson.fromJson(reader, listType);
+
+            return (list != null) ? list : new ArrayList<>();
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar.");
+            return new ArrayList<>();
+        }
+    }
 }
