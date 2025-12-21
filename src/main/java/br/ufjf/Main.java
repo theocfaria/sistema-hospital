c1e683191f05d9f39a8a560628c0fa4e745c0007
Theo Faria <theocfaria@gmail.com>
2025-12-21 15:00:48 -0300

    chore: estrutura o projeto

---
+++ src/main/java/br/ufjf/Main.java
@@ -0,0 +1,9 @@
+package br.ufjf;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        int teste = 5;
+        System.out.println(teste);
+    }
+}
