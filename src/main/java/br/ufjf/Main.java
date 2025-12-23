2fbc16b0748983cec8d2df4421d8453560365082
RyanPAlvim <ryalvim84@gmail.com>
2025-12-23 19:20:02 -0300

    feat(factories): cria factory para recepcionista e uma factory principal com o atributo range.

--- src/main/java/br/ufjf/Main.java
+++ src/main/java/br/ufjf/Main.java
@@ -1,11 +1,11 @@
 package br.ufjf;
 
+import br.ufjf.factory.Factory;
 import br.ufjf.factory.MedicoFactory;
 import br.ufjf.factory.PacientFactory;
 
 public class Main {
     public static void main(String[] args) {
-        PacientFactory.populate();
-        MedicoFactory.populate();
+        Factory.populate(3);
     }
 }
