b0f6ab81db2ac4518c62b5ee20c90d9e134f9aab
Murilo19Dz9 <murilolacerdadutra19@gmail.com>
2025-12-23 17:21:43 -0300

    chore: mudança do pom para adicionar JavaFX

---
+++ src/main/java/br/ufjf/controller/LoginController.java
@@ -0,0 +1,14 @@
+package br.ufjf.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML private TextField TxtCpf;
+    @FXML private TextField TxtPassword;
+    @FXML private Button BtnLogin;
+    @FXML private Button BtnClose;
+
+
+}
