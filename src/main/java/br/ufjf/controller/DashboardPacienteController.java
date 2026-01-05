0b57a8525780ebc1baf301dad404c1aff3b2d9c1
Theo Faria <theocfaria@gmail.com>
2026-01-05 10:43:17 -0300

    refact(paciente): renomeia .fxml do dashboard, cria arquivos dos contents

---
+++ src/main/java/br/ufjf/controller/DashboardPacienteController.java
@@ -0,0 +1,57 @@
+package br.ufjf.controller;
+
+import br.ufjf.model.User;
+import br.ufjf.navigation.ChangeScreen;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class DashboardPacienteController implements DashboardController, ChangeScreen {
+    @FXML
+    private AnchorPane content;
+    @FXML
+    private Label LblUsername;
+    @FXML
+    private Button BtnLogout;
+    @FXML
+    private Button BtnConsulta;
+    @FXML
+    private Button BtnDocumentos;
+    @FXML
+    private Button BtnPerfil;
+    @FXML
+    private Button BtnVisita;
+    private User user;
+
+    @Override
+    public void setUser(User user) {
+        this.user = user;
+        LblUsername.setText("Olá, " + user.getName());
+    }
+
+    @FXML
+    public void agendarConsultas() {
+        carregarContent(user, content, "paciente/agendarConsultas.fxml");
+    }
+
+    @FXML
+    public void acessarDocumentos() {
+        carregarContent(user, content, "paciente/acessarDocumentos.fxml");
+    }
+
+    @FXML
+    public void historicoEDados() {
+        carregarContent(user, content, "paciente/historicoEDados.fxml");
+    }
+
+    @FXML
+    public void checarVisitas() {
+        carregarContent(user, content, "paciente/checarVisitas.fxml");
+    }
+
+    @FXML
+    public void logout() {
+        trocarTela(BtnLogout, "Login.fxml");
+    }
+}
